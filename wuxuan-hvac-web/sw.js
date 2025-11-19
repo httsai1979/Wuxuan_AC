e877de74ceea8e5bf2998cdca8ddab1e97409310
httsai1979 <138438983+httsai1979@users.noreply.github.com>
2025-11-19 15:48:08 +0000

    Add Hualien zoning settings and info cards

---
+++ wuxuan-hvac-web/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'wuxuan-hvac-cache-v1';
+const PRECACHE_URLS = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  if (request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      const networkFetch = fetch(request)
+        .then((response) => {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, clone));
+          return response;
+        })
+        .catch(() => cached);
+      return cached || networkFetch;
+    })
+  );
+});
